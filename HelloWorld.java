11355d7e3e3c34100e4d468cdb9e1b144b953fb5
T2K-PP <xiaob.zhu@foxmail.com>
2020-03-10 14:35:48 +0800

    2th 提交

--- HelloWorld.java
+++ HelloWorld.java
@@ -3,5 +3,6 @@ public class HelloWorld{
 	public static void main(String[] args){
 		System.out.println("Helloword");		
 		System.out.println("Helloword2");		
+		System.out.println("Helloword2");
 	}
 }
